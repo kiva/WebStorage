5cf32ffda45cceab26b3f61f53a2c3be0e0daa6c
Aswin Venkata <aswin@kiva.org>
2014-07-07 12:32:15 -0700

    add code for all the functions + write unit tests for them

--- src/WebStorage.js
+++ src/WebStorage.js
@@ -30,28 +30,39 @@ function WebStorage(store){
 
 
 WebStorage.prototype = {
-	get: function(){
-	
-	}
-
-
-	,set: function(){
+	set: function(key, value){
+		var str_value = JSON.stringify(value);
+		str_value = btoa(str_value); 
+		this.store.setItem(key,str_value);
 
 	}
 
+	,get: function(key){
+		var raw_value = this.store.getItem(key);
+		if(raw_value === null){
+			return null;
+		}
+		var enc_value = atob(raw_value); 
+		return JSON.parse(enc_value);
+	}
 
-	,rm: function(){
-	
+
+	,rm: function(key){
+		this.store.removeItem(key);
 	}
 
 
 	,flush: function(){
-
+		this.store.clear();
 	}
 
 
 	,getAll: function(){
-
+		var store_values = {};
+		for(var key in this.store){
+			store_values[key] = this.get(key);
+		}
+		return store_values;
 	}
 };
 
--- test/spec/WebStorage.js
+++ test/spec/WebStorage.js
@@ -32,7 +32,7 @@ describe('WebStorage', function() {
 	});
 
 
-	describe('.set', function () {
+	describe('.set()', function () {
 		var webStore;
 
 
@@ -43,12 +43,21 @@ describe('WebStorage', function() {
 
 		it('sets obfuscated data in the store', function () {
 			webStore.set('test', 'jellybeans');
-			expect(localStorage.getItem('test')).toBe('amVsbHliZWFucw==');
+			expect(localStorage.getItem('test')).toBe('ImplbGx5YmVhbnMi');
+			// make sure we get back the exact data types.
+			webStore.set('key_1','this is a string');
+			expect(webStore.get('key_1')).toBeString();
+			webStore.set('key_2',491);
+			expect(webStore.get('key_2')).toBeNumber();
+			webStore.set('key_3',false);
+			expect(webStore.get('key_3')).toBeBoolean();
+			webStore.set('key_4',{some_key:'some_value'});
+			expect(webStore.get('key_4')).toBeObject();
 		});
 	});
 
 
-	describe('.get', function () {
+	describe('.get()', function () {
 		var webStore;
 
 
@@ -58,8 +67,67 @@ describe('WebStorage', function() {
 
 
 		it('gets data from the store and de-obfuscates it', function () {
-			localStorage.setItem('test', 'amVsbHliZWFucw==');
+			localStorage.setItem('test', 'ImplbGx5YmVhbnMi');
 			expect(webStore.get('test')).toBe('jellybeans');
 		});
 	});
+
+	
+	describe('.rm()', function () {
+		var webStore;
+
+
+		beforeEach(function () {
+			webStore = new WebStorage('localStorage');
+		});
+
+
+		it('removes value from store', function () {
+			webStore.set('key_1','value_1');
+			webStore.rm('key_1');
+			expect(webStore.get('key_1')).toBeNull();
+
+		});
+	});	
+
+
+	describe('.flush()', function () {
+		var webStore;
+
+
+		beforeEach(function () {
+			webStore = new WebStorage('localStorage');
+		});
+
+
+		it('removes all values from the store', function () {
+			webStore.set('key_1','value_1');
+			webStore.set('key_2','value_2');
+			webStore.flush();
+			expect(webStore.get('key_1')).toBeNull();
+			expect(webStore.get('key_1')).toBeNull();
+			expect(localStorage.length).toBe(0);
+
+		});
+	});
+
+
+	describe('.getAll()', function () {
+		var webStore;
+
+
+		beforeEach(function () {
+			webStore = new WebStorage('localStorage');
+		});
+
+
+		it('gets all values as an object', function () {
+			webStore.set('key_1','value_1');
+			webStore.set('key_2','value_2');
+			var obj = webStore.getAll();
+			expect(obj.key_1).toBe('value_1');
+			expect(obj.key_2).toBe('value_2');
+
+		});
+	});		
 });
